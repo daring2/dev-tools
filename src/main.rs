862f6a637866f128cf70bd87228841405649cf81
daring <danila.daring@gmail.com>
2023-09-25 23:14:59 +0300

    parse command args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args().skip(1);
+    let command: &str = &args.next().unwrap();
+    match command {
+        "git-release" => perform_git_release(args),
+        _ => ()
+    };
+}
+
+fn perform_git_release(mut args: impl Iterator<Item = &str>) {
+    //TODO implement
 }
