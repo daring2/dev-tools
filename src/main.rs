e8c4e9a9bf4c50c8819a00c1ae1277fd0f026839
daring <danila.daring@gmail.com>
2023-09-27 12:24:48 +0300

    invoke cmd script

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
     let mut args = env::args().skip(1);
     let command: &str = &args.next().unwrap();
     match command {
-        "release" => perform__release(args),
+        "release" => perform_release(args),
         _ => ()
     };
 }
 
-fn perform__release(mut args: impl Iterator<Item = String>) {
+fn perform_release(mut args: impl Iterator<Item = String>) {
     let new_version = args.next().unwrap();
-    println!("new version: {new_version}");
+    Command::new("cmd")
+        .args(["/C", "test.bat p1 p2"])
+        .current_dir(env::current_dir().unwrap())
+        .status()
+        .unwrap();
 }
