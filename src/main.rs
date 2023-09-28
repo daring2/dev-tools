ffec23d9d090b9894580e9308a44f519ec256a3b
daring <danila.daring@gmail.com>
2023-09-28 15:42:09 +0300

    read current version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::env;
+use std::{env, io};
+use std::fs;
 use std::process::Command;
 
 fn main() {
@@ -11,10 +12,23 @@ fn main() {
 }
 
 fn perform_release(mut args: impl Iterator<Item = String>) {
-    let new_version = args.next().unwrap();
-    Command::new("cmd")
-        .args(["/C", "test.bat p1 p2"])
+    let current_version = read_current_version();
+    let next_version = args.next().unwrap();
+    println!("release: current_version={}, next_version={}", current_version, next_version);
+    exec_cmd("gradlew.bat clean build publish");
+}
+
+fn read_current_version() -> String {
+    let content = fs::read_to_string("gradle.properties").unwrap();
+    let version = content.strip_prefix("version=").unwrap().trim();
+    return String::from(version)
+}
+
+fn exec_cmd(command: &str) {
+    let status = Command::new("cmd")
+        .args(["/C", command])
         .current_dir(env::current_dir().unwrap())
         .status()
         .unwrap();
+    assert!(status.success());
 }
