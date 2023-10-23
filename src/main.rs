f36563a8a6436daa281f5ec7cb36de8e3b6aa177
daring <danila.daring@gmail.com>
2023-10-23 21:24:29 +0300

    replace panic with Result

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,20 @@ fn main() {
 
 type CmdResult<T> = Result<T, String>;
 
-fn perform_release(mut args: impl Iterator<Item = String>) -> CmdResult<i8> {
+fn perform_release(mut args: impl Iterator<Item = String>) -> CmdResult<()> {
     let current_version = load_current_version()?;
     let next_version = args.next()
-        .expect("Please specify next version");
+        .ok_or("Please specify next version")?;
     println!("release: current_version={current_version}, next_version={next_version}");
     //TODO add option for clean build
     let gradle_cmd = "gradlew.bat --no-daemon";
     exec_cmd(&format!("{gradle_cmd} publish"));
     exec_cmd(&format!("git tag -a v{0} -m v{0}", current_version));
     fs::write("gradle.properties", format!("version={next_version}"))
-        .expect("Cannot update version");
+        .map_err(|e|format!("Cannot update version: {e}"))?;
     //TODO use "build version {current_version} message
     exec_cmd(&format!("git commit -am \"build version {current_version}\""));
-    Ok(0)
+    Ok(())
 }
 
 fn load_current_version() -> CmdResult<String> {
