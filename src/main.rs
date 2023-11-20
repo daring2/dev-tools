d8d29255d19480a26e7edd7d9bbc49ff42f1571e
daring <danila.daring@gmail.com>
2023-11-20 23:47:21 +0300

    use anyhow crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{env, process};
+use std::env;
 use std::fs;
 use std::os::windows::process::CommandExt;
 use std::process::Command;
+
+use anyhow::{anyhow, bail, Context, Result};
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
@@ -23,15 +25,12 @@ struct ReleaseArgs {
     //TODO add options current_version, clean, build, etc
 }
 
-fn main() {
-    let result = execute();
-    if let Err(e) = result {
-        eprintln!("{e}");
-        process::exit(1);
-    }
-}
+type CmdResult<T> = Result<T>;
+// type CmdResult<T> = Result<T, String>;
 
-type CmdResult<T> = Result<T, String>;
+fn main() -> CmdResult<()> {
+    execute()
+}
 
 fn execute() -> CmdResult<()> {
     let cli = Cli::parse();
@@ -44,7 +43,8 @@ fn perform_release(args: ReleaseArgs) -> CmdResult<()> {
     let current_version = load_current_version()?;
     let next_version = args.next_version;
     if next_version.trim().is_empty(){
-        return Err("Please specify next version".to_string());
+        bail!("Please specify next version");
+        // return Err("Please specify next version".to_string());
     }
     println!("release: current_version={current_version}, next_version={next_version}");
     //TODO add option for clean build
@@ -53,7 +53,8 @@ fn perform_release(args: ReleaseArgs) -> CmdResult<()> {
     exec_cmd(&format!("git tag -a v{0} -m v{0}", current_version))?;
     //TODO update only version property
     fs::write("gradle.properties", format!("version={next_version}"))
-        .map_err(|e|format!("Cannot update version: {e}"))?;
+        .context("Cannot update version")?;
+        // .map_err(|e|format!("Cannot update version: {e}"))?;
     //TODO use "build version {current_version} message
     exec_cmd(&format!("git commit -am \"build version {current_version}\""))?;
     Ok(())
@@ -62,30 +63,32 @@ fn perform_release(args: ReleaseArgs) -> CmdResult<()> {
 fn load_current_version() -> CmdResult<String> {
     let file = "gradle.properties";
     fs::read_to_string(file)
-        .map_err(|e|format!("Cannot read file '{file}': {e}"))?
+        .with_context(||format!("Cannot read file '{file}'"))?
+        // .map_err(|e|format!("Cannot read file '{file}': {e}"))?
         .lines()
         .filter_map(|it|it.strip_prefix("version="))
         .map(|it|String::from(it.trim()))
         .next()
-        .ok_or(format!("Cannot find current version property"))
+        .context("Cannot find current version property")
+        // .ok_or(format!("Cannot find current version property"))
 }
 
 fn exec_cmd(command: &str) -> CmdResult<()> {
-    let current_dir = env::current_dir().map_err(
-        |e|format!("Cannot get current_dir: {e}")
-    )?;
+    let current_dir = env::current_dir()
+        .context("Cannot get current_dir")?;
+        // .map_err(|e|format!("Cannot get current_dir: {e}"))?;
     println!("execute {command}");
     Command::new("cmd").arg("/C")
         .raw_arg(command)
         .current_dir(current_dir)
         .status()
-        .map_err(|e|e.to_string())
+        .map_err(|e|anyhow!(e))
+        // .map_err(|e|e.to_string())
         .and_then(|it| match it {
             _ if it.success() => Ok(it),
-            _ => Err(it.to_string())
+            _ => Err(anyhow!(it))
         })
-        .map_err(|e|
-            format!("Cannot execute command '{command}': {e}")
-        )?;
+        .with_context(||format!("Cannot execute command '{command}'"))?;
+        // .map_err(|e|format!("Cannot execute command '{command}': {e}"))?;
     Ok(())
 }
